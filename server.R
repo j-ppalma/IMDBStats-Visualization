c47da2d992281c52cd0e8a1deadcf3b02b0c22f2
j-ppalma <jp.palma@campus.fct.unl.pt>
2022-06-09 16:32:38 +0100

    all

---
+++ server.R
@@ -0,0 +1,138 @@
+
+library(ggplot2)
+library(viridis)
+library(dplyr)
+library(stringr)
+library(plotly)
+library(DT)
+library(forcats)
+function(input, output) {
+  #################DATAFRAMA VIS1###################
+ 
+   datafilter<-reactive({
+     df1<-df[str_detect(df$genres,input$genre),]
+     df1 <- df1%>%
+       arrange(desc(imdb_score)) %>%
+       head(n = 20L)
+     })
+   global <- reactiveValues(selectedValTable = "Click a Row to View the Movie/Show Description.", 
+                            selectedBar = "The Walking Dead")
+  
+  
+   #################Value Box1###################
+   
+  numVotes<-reactive({
+    votes<-df1%>% filter(title==global$selectedBar)%>%select( imdb_votes)
+    votes[1,"imdb_votes"]
+  })
+   output$votes <- renderValueBox({valueBox(numVotes(), "IMDB Votes",color = "red",
+                                            icon =icon("thumbs-up", lib = "glyphicon"),width = 12)})
+   #################Value Box2###################
+   
+   numScore<-reactive({
+     score<-df1%>% filter(title==global$selectedBar)%>%select(imdb_score)
+     score[1,"imdb_score"]
+   })
+  output$score <- renderValueBox({valueBox(numScore(), "IMDB Value",color = "yellow",
+                                             icon =icon("thumbs-up", lib = "glyphicon"),width = 12)})
+   
+   
+   output$text <- renderUI({
+     req(global$selectedBar)
+     textInput(inputId = "label", label = "IMDB Votes & Values Of The Selected Bar Element:", value = global$selectedBar)
+   })
+   
+   
+   #################Visualization 1###################
+   
+   
+  output$plot1<-
+    renderPlot({
+      asd<-datafilter()
+      asd<-asd%>%mutate(title = fct_reorder(title, desc(imdb_score)))
+      observeEvent(input$plot1_click, {
+        global$selectedBar <- asd$title[round(input$plot1_click$y)]
+        })
+    ggplot(asd, aes(y=title, x=imdb_score, fill=type,color="", width=.75)) +
+      geom_bar(stat="identity")+scale_fill_manual(values = c("#feb24c", "#f03b20"))+
+    scale_color_manual(values = "black")+theme_bw()+
+      theme(title = element_text(size=10),axis.text.x = element_text(size = 7),
+            axis.text.y = element_text(size = 10))+
+      ggtitle(paste("The 20",{input$genre},"best rated Movies/Shows"))+ ylab(NULL)+
+      xlab(NULL)+guides(color = FALSE)
+    
+    })
+  
+
+  #################Dataframe 2###################
+   df2<-titles %>% select(-id,-description,-imdb_id)
+   
+  #################Visualization 2################
+   observeEvent(input$mytable_rows_selected, {
+      desc<-df1%>%filter(grepl(df2[input$mytable_rows_selected,1],title,fixed = TRUE))%>%select(description)
+     global$selectedValTable<-substring(desc,1)
+
+   })
+   
+   output$description = renderPrint({
+    global$selectedValTable
+   })
+   output$mytable = renderDataTable(
+     df2,selection="single"
+   
+   )
+   
+   #################Dataframe 3###################
+   df3<-titles %>% filter(type=="SHOW")%>% 
+     select(title,seasons,tmdb_popularity,imdb_score,tmdb_score,imdb_votes)
+   df3<-na.omit(df3) 
+   df4<-df3
+   df4<-df4%>% select(-title)%>%group_by(seasons)%>%
+     summarize(n = n())
+   df4$seasons[df4$seasons >= 9] <- "more than 9"  
+   df4$seasons<-paste(df4$seasons," seasons")
+   output$plot3<-
+     renderPlotly({
+   plot3 <- plot_ly(df4, labels = ~seasons, values = ~n, type = 'pie',textposition = 'inside',textinfo = 'label+percent',
+                    showlegend=FALSE,marker=list(colors = RColorBrewer::brewer.pal(9, "Spectral")))
+   plot3 <- plot3 %>% layout(title = 'Amount Of Series By Number Of Seasons',
+                             width = 500, height = 500,
+                         xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                         yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))%>%
+     event_register("plotly_click")
+   
+ 
+   plot3
+    })
+   
+   output$Pieclick <- renderPlot({
+     d <- event_data("plotly_click")$pointNumber
+     x<-if(is.null(d))10 else as.numeric(d)
+     
+     if(x==10){
+    df5<-df3%>%filter(seasons>=x)
+    titl<-"more than 10"
+   }else{
+     df5<-df3%>%filter(seasons==x+1)
+     titl<-x+1
+   }
+     df5<-df5%>%
+       arrange(desc(tmdb_popularity)) %>%
+       head(n = 10L)
+     show(df5)
+     
+     ggplot(df5, aes(y=title, x=tmdb_popularity, fill=title)) +
+       geom_bar(stat="identity")+theme_minimal()+coord_polar()+                                              # Change color brewer palette
+       scale_fill_brewer(palette = "Spectral")+ xlab(NULL)+ ylab(NULL)+
+       geom_text(aes(x = 0, label = title),position = position_dodge(width=0.9),  size=5) +
+       theme(axis.text.y = element_blank(),legend.position = "none",title = element_text(size=13))+ggtitle(paste("The 10 Most Popular Series Of The Slice Selected.(",titl,")"))+
+       scale_x_continuous(breaks = seq(0, 1000, by=100), limits=c(0,1000))
+
+     
+     
+   })
+   
+   
+   
+
+}
---
+++ ui.R
@@ -0,0 +1,79 @@
+library(shinydashboard)
+library(plotly)
+
+header <- dashboardHeader(title = "IMDBStat")
+
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("HomePage", tabName = "hp", icon = icon("home", lib = "glyphicon")),
+    menuItem("IMDB Best Rated Movies/Shows", icon = icon("stats", lib = "glyphicon"), tabName = "vis1"),
+    menuItem("IMDB Datatable", icon = icon("stats", lib = "glyphicon"), tabName = "vis2"),
+    menuItem("Series Pie Charts", icon = icon("stats", lib = "glyphicon"), tabName = "vis3")
+    
+    
+  )
+)
+
+body <- dashboardBody(  # Boxes need to be put in a row (or column)
+  fluidRow(
+    
+    tabItems(
+      tabItem(tabName = "hp",
+              h2(align="center","IMDBStat - Data Visualization Assignment 4" ),
+              box(align="center", width = 6,div(tags$img(src = "putlogo.png", width="300px"))),
+              box(height="320",align="center",h4("IMDBStat is a dashboard that uses a IMDB dataset taken from kaggle, which will be available on the gitup, for the purpose of facilitating the analysis of the movies success or unsuccess and accessing some statistics in an easy way. Its was develloped as the assignment 4 of Data Visualization, it was made individually, and it would be great to have that in consideration while evaluating it."))
+      ),
+      tabItem(
+        tabName = "vis1",
+        h2(align="center","IMDB Best Rated Movies/Shows"),
+        box(selectInput("genre", h3("Select genre:"), choices = list("Action" = "action","Animation"="animation",
+                                                                     "Comedy"="comedy","Crime"="crime", "Drama" = "drama",
+                                                                     "Documentation" = "documentation","European"="european",
+                                                                     "Family"="family","Fantasy"="fantasy",
+                                                                     "Horror"="horror","Music"="music","Romance"="romance",
+                                                                     "Scifi"="scifi","Sport"="sport","Thriller"="thriller",
+                                                                     "War"="war","Western"="western" ), selected = "action"),width = 4,height = 220),
+        
+      
+      
+        box(
+          uiOutput("text"),
+          valueBoxOutput("votes",width = 6),
+          valueBoxOutput("score",width = 6),
+          width = 8,
+          height = 220
+        ),
+        box(plotOutput("plot1",height = 500,click = "plot1_click"),width = 12,height = 520)
+    ),
+    tabItem(
+      tabName = "vis2",
+      box(h2(align="center","IMDB Datatable",width=12),width = 6,height = 200,status = "danger", solidHeader = FALSE),
+      box(
+        h3(align="center","Description",width=12),
+        textOutput("description"),width = 6,height = 200,status = "danger", solidHeader = TRUE),
+      basicPage(
+        DT::dataTableOutput("mytable")
+      )
+      
+    ),
+    tabItem(
+      tabName="vis3",
+      box(h2(align="center","Shows Pie Chart",width=12),width = 12,height = 100,status = "danger", solidHeader = FALSE),
+      box(plotlyOutput("plot3",height =500 ),width = 6,height = 520, align = "center"),
+      box(plotOutput("Pieclick",width = 500,height=500),width = 6,height = 520,align="center" )
+    )
+    
+    )
+    
+    
+  ))
+
+
+dashboardPage(
+  skin="red",
+  header,
+  sidebar,
+  body
+  
+)
